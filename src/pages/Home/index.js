541b709780da0193dc5f26b73975db73656196a8
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-19 16:56:52 -0300

    Criando a tela Home e o componente historico

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,36 @@
-import { Text, View, Button } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Background, Container, Nome, Saldo, Title, List } from "./styles";
 
 import { AuthContext } from "../../contexts/auth";
 
+import HistoricoList from "../../components/HistoricoList";
+
 const Home = () => {
-  const { user, signOut } = useContext(AuthContext);
+  const [historico, setHistorico] = useState([
+    {key: '1', tipo: 'receita', valor: 1200},
+    {key: '2', tipo: 'despesa', valor: 200},
+    {key: '3', tipo: 'receita', valor: 2200},
+    {key: '4', tipo: 'despesa', valor: 1000},
+    {key: '5', tipo: 'receita', valor: 1200},
+    {key: '6', tipo: 'despesa', valor: 200},
+    {key: '7', tipo: 'receita', valor: 2200},
+    {key: '8', tipo: 'despesa', valor: 1000},
+  ])
+  const { user } = useContext(AuthContext);
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>{user && user.nome}</Text>
-      <Text>{user && user.email}</Text>
-      <Button title="Sair da conta" onPress={() => signOut()} />
-    </View>
+    <Background>
+      <Container>
+        <Nome>Ricardo</Nome>
+        <Saldo>R$ 12000,00</Saldo>
+      </Container>
+      <Title>Últimas movimentações</Title>
+      <List
+      showsVerticalScrollIndicator={false}
+      data={historico}
+      keyExtractor={item => item.key}
+      renderItem={({item}) => (<HistoricoList data={item} />)}
+      />
+    </Background>
   );
 };
 
